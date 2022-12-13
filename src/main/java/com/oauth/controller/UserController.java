72152c6fcf6a902186ba9b6d0b6e777d7e30821b
khghouse <khghouse@naver.com>
2022-12-13 18:28:36 +0900

    create UserController

---
+++ src/main/java/com/oauth/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.oauth.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/user")
+    public List listUser() {
+        return userService.findAll();
+    }
+
+    @PostMapping("/user")
+    public User create(@RequestBody User user) {
+        return userService.save(user);
+    }
+
+    @DeleteMapping("/user/{id}")
+    public String delete(@PathVariable(value = "id") Long id) {
+        userService.delete(id);
+        return "success";
+    }
+}
+
